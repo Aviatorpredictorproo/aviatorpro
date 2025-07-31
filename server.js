f3997c8cfff1877f6088b3331a0255830fdde7a3
Aviatorpredictorproo <simonpeter3055@gmail.com>
2025-07-31 23:38:30 +0300

    Update login credentials

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    if (username === 'Megatech' && password === 'admin@2025') {
+    if (username === 'Megatech' && password === 'admin@admin') {
         req.session.loggedIn = true;
         return res.redirect('/dashboard');
     }
